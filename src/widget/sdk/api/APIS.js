3332ce2e3a4eba91c3039d112b2b047d3201a3bf
drishya chhetri <drishyachhetri96@gmail.com>
2020-05-26 11:01:39 +0545

    sdk wrap up

--- src/widget/sdk/api/APIS.js
+++ src/widget/sdk/api/APIS.js
@@ -1,7 +1,6 @@
-// export const host_ip_address = "http://localhost:8000/";
-// const host = "http://localhost:8000/api/v2/";
-export const host_ip_address = "https://www.khalti.com";
-const host = "https://www.khalti.com/api/v2/";
+//export const host_ip_address = "http://localhost:8000/";
+export const host_ip_address = "https://www.khalti.com/";
+const host = host_ip_address + "api/v2/";
 export const initiation_api = host + "payment/initiate/";
 export const confirmation_api = host + "payment/confirm/";
 export const verification_api = host + "payment/verify/";
@@ -9,9 +8,8 @@ export const verification_api = host + "payment/verify/";
 export const ebanking_initiation_api = host + "ebanking/initiate/";
 
 export const mobile_banking_list =
-  "https://www.khalti.com/api/bank/?has_mobile_banking=true";
-export const ebanking_list =
-  "https://www.khalti.com/api/bank/?has_ebanking=true";
+  host_ip_address + "api/bank/?has_mobile_banking=true";
+export const ebanking_list = host_ip_address + "api/bank/?has_ebanking=true";
 
 export const queryToString = (params) => {
   return Object.keys(params)
