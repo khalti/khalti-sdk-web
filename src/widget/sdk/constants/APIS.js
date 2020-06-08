06dfad199dc42325759259d05276ae28ace22474
manoj <chapagainmanoj35@gmail.com>
2020-06-08 16:42:54 +0545

    inititate url updated

--- src/widget/sdk/constants/APIS.js
+++ src/widget/sdk/constants/APIS.js
@@ -8,7 +8,7 @@ export const host_ip_address = KHALTI_SERVER;
 export const initiation_api = KHALTI_SERVER + "/api/v2/payment/initiate/";
 export const confirmation_api = KHALTI_SERVER + "/api/v2/payment/confirm/";
 export const verification_api = KHALTI_SERVER + "/api/v2/payment/verify/";
-export const ebanking_initiation_api = KHALTI_SERVER + "/v2/ebanking/initiate/";
+export const ebanking_initiation_api = KHALTI_SERVER + "/ebanking/initiate/";
 export const mobile_banking_list =
   KHALTI_SERVER + "/api/v2/bank/?has_mobile_banking=true";
 export const ebanking_list = KHALTI_SERVER + "/api/v2/bank/?has_ebanking=true";
